9b39075b9b457c91218ba1ae6ad8fad31a2b5b8c
Lars Boegild Thomsen <lth@netcompartner.com>
2016-03-24 11:22:51 +0800

    Hmmm

---
+++ run.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/*
+ * Any Package Manager
+ *
+ * Copyright (C) 2016 Lars Boegild Thomsen <lth@cow.dk>
+ *
+ */
+console.log("Hello World!");
+/*
+ * vim: ts=4 et nowrap autoindent
+ */
